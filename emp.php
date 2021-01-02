617124eca82c9a4e16bb1805edf3f7d9e0288abc
yogirajsinh96 <42890006+yogirajsinh96@users.noreply.github.com>
2021-01-02 18:12:06 +0530

    Update emp.php

--- emp.php
+++ emp.php
@@ -1,3 +1,46 @@
-<html>
+<?php
 
+	if (isset($_POST['submit'])){
+		
+	}
+
+?>
+
+<html>
+<head>
+	<title>Employee Details</title>
+</head>
+<body>
+	<center>
+		<form>
+		<table>
+			<tr>
+			<td>
+		Name : </td><td><input id="emp_name" name="emp_name" type="text" /><td></tr>
+<tr>
+			<td>
+			Mobile Number : </td><td><input id="emp_name" name="emp_name" type="text" /><td></tr>
+			<tr>
+			<td>
+			Email :</td><td> <input id="emp_name" name="emp_name" type="text" /><td></tr>
+			<tr>
+			<td>
+			Age :</td><td> <input id="emp_name" name="emp_name" type="text" /><td></tr>
+			<tr>
+			<td>
+			Employee Exp. : </td><td><input id="emp_name" name="emp_name" type="text" /><td></tr>
+			<tr>
+			<td>
+			Employee skill :</td><td> <input id="emp_name" name="emp_name" type="text" /><td></tr>
+			<tr>
+			<td>
+			Resume :</td><td> <input id="emp_name" name="emp_name" type="text" /><td></tr>
+			
+					<tr>
+			<td></td><td>
+			<input type="submit" name="submit" /><td></tr>
+			</table>
+		</form>
+	</center>
+</body>
 </html>
